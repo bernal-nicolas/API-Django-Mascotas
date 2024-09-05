222c5d7de7167b91094e183905737cca00fd1c06
Nicolás Bernal <nicolasbernal.cc@gmail.com>
2024-09-05 13:50:57 -0500

    Cambios Parametro Genero Mascotas

--- api/models.py
+++ api/models.py
@@ -4,14 +4,22 @@ from django.db import models
 
 
 class Mascota(models.Model):
+    GENERO_CHOICES = [
+        ('masculino', 'Masculino'),
+        ('femenino', 'Femenino'),
+    ]
     nombre = models.CharField(max_length=50)
     raza = models.CharField(max_length=50)
-    genero = models.CharField(max_length=50)
+    genero = models.CharField(max_length=10, choices=GENERO_CHOICES)
     fecha_nacimiento = models.DateField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
     
 class RegistrosBase(models.Model):
+    GENERO_CHOICES = [
+        ('masculino', 'Masculino'),
+        ('femenino', 'Femenino'),
+    ]
     raza = models.CharField(max_length=50)
     peso_minimo = models.DecimalField(max_digits=5, decimal_places=2)
     peso_maximo = models.DecimalField(max_digits=5, decimal_places=2)
-    genero = models.CharField(max_length=50)
+    genero = models.CharField(max_length=10, choices=GENERO_CHOICES)
